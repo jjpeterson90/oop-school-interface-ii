5a0f14a6a8cc4167fc572a16c88b3fa24d9aec6e
Justin <jjpeterson90@hotmail.com>
2022-06-05 17:55:39 -0500

    complete

--- classes/school.py
+++ classes/school.py
@@ -6,3 +6,15 @@ class School:
         self.name = name
         self.staff = Staff.objects()
         self.students = Student.objects()
+
+    def list_students(self):
+        student_count = 1
+        print("\n")
+        for each_student in self.students:
+            print(f"{student_count}. {vars(each_student)['name']} {vars(each_student)['school_id']}")
+            student_count += 1
+            
+    def find_student_by_id(self, student_id):
+        for each_student in self.students:
+            if vars(each_student)['school_id'] == student_id:
+                print(each_student)
--- runner.py
+++ runner.py
@@ -1,8 +1,21 @@
+from tkinter import Menu
 from classes.school import School
 
 school = School('Ridgemont High')
 
+menu_prompt = "\nWhat would you like to do?\nOptions:\n1. List All Students\n2. View Individual Student <student_id>\n3. Add a Student\n4. Remove a Student <student_id>\n5. Quit\n"
 
-print(school.staff)
-print(school.students)
+# mode = input(menu_prompt)
 
+while True:
+    mode = input(menu_prompt)
+    if mode == '1':
+        school.list_students()
+    elif mode == '2':
+        student_id = input('Enter a student id:')
+        school.find_student_by_id(student_id)
+    elif mode == '5':
+        break
+
+# print(school.staff)
+# print(school.students)
